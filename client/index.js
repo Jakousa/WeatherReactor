ef2c66e18f4d663120d1c163a3075d9f366f4775
Jakousa <japekohatcher93@hotmail.com>
2018-02-25 20:24:41 +0200

    hydrate in production

--- client/index.js
+++ client/index.js
@@ -1,12 +1,19 @@
 import 'babel-polyfill'
 
 import React from 'react'
-import { render } from 'react-dom'
+import { render, hydrate } from 'react-dom'
 import App from './components/App'
 
 if (typeof window !== 'undefined') {
-    render(
-        <App />,
-        document.getElementById('app'),
-    )
+    if (process.env.NODE_ENV === 'production') {
+        hydrate(
+            <App />,
+            document.getElementById('app'),
+        )
+    } else {
+        render(
+            <App />,
+            document.getElementById('app'),
+        )
+    }
 }
